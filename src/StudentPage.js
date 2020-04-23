0c8c2f985436eefef43f91718c946977baf3a8e2
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-23 22:23:22 +0200

    add initial student page and get student name from parameter

--- src/StudentPage.js
+++ src/StudentPage.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ChartComponent from './components/ChartComponent';
 
 class StudentPage extends React.Component {
-    constructor() {
-        super()
-        this.state = {}
+    constructor(props) {
+        super(props)
+        this.state = {
+            studentName: props.match.params.name
+        }
     }
 
     render() {
+        // console.log('name', this.state.studentName)
+        const studentData = this.props.studentData.filter(item => {
+            return item.name === this.state.studentName
+        })
         return (
             <div className="StudentPage">
                 <h1>Students</h1>
-                <ChartComponent />
+                <ChartComponent
+                    studentData={studentData}
+                />
             </div>
         )
     }
