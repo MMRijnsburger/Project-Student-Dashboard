85ef4b9a831ad3846bf12ca3f02f9635a958623c
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-24 11:19:16 +0200

    add links to StudentsPage Component

--- src/pages/StudentsPage.js
+++ src/pages/StudentsPage.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
 
-const StudentsPage = () => {
+const StudentsPage = (props) => {
+    const studentLinks = props.students.map(item => {
+        const studentUrl = "/Students/Student/" + item
+        return (
+            <li key={item}>
+                <Link to={studentUrl}>{item}</Link>
+            </li>
+        )
+    })
     return (
         <div className="StudentsPage">
             <h1>Students</h1>
             TODO create links to /students/student/name
             The students are in props.students
             this is an array, string array, of student names
+            <Router>
+                <ul>
+                    {studentLinks}
+                </ul>
+            </Router>
         </div>
     )
 }
